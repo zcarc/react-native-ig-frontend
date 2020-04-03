9f911a8342e914659e9e9ecd2a133426b3faf001
wrkbr <zcarc1@gmail.com>
2020-04-03 20:30:47 +0900

    #4.1 GlobalStyles and Theme

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {ThemeProvider} from 'styled-components';
+import GlobalStyles from '../Styles/GlobalStyles';
+import Theme from '../Styles/Theme';
 
-function App() {
-  return (
-    <div className="App"/>
-  );
-}
+export default () => (
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+  </ThemeProvider>
+);
 
-export default App;
---
+++ src/Styles/GlobalStyles.js
@@ -0,0 +1,11 @@
+import {createGlobalStyle} from 'styled-components';
+import reset from 'styled-reset';
+
+export default createGlobalStyle`
+    ${reset};
+
+    * {
+        box-sizing: border-box;
+    }
+    
+`;
