6944f9f6ac4b8729efd91231f9c4e9915d9b7c99
wrkbr <zcarc1@gmail.com>
2020-04-06 22:00:26 +0900

    #7.3 Post Component part Three

--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import useInput from '../../Hooks/useInput';
 import PostPresenter from './PostPresenter';
@@ -16,7 +16,22 @@ const PostContainer = ({
 }) => {
   const [isLikedS, setIsLiked] = useState(isLiked);
   const [likeCountS, setLikeCount] = useState(likeCount);
+  const [currentItem, setCurrentItem] = useState(0);
   const comment = useInput("");
+  
+  const slide = () => {
+    const totalFiles = files.length;
+    if (currentItem === totalFiles - 1) {
+      setTimeout(() => setCurrentItem(0), 3000);
+    } else {
+      setTimeout(() => setCurrentItem(currentItem + 1), 3000);
+    }
+  };
+
+  useEffect(() => {
+    slide();
+  }, [currentItem]);
+
   return (
     <PostPresenter
       user={user}
@@ -28,19 +43,18 @@ const PostContainer = ({
       newComment={comment}
       setIsLiked={setIsLiked}
       setLikeCount={setLikeCount}
+      currentItem={currentItem}
     />
   );
 };
 
 PostContainer.propTypes = {
   id: PropTypes.string.isRequired,
-  user: PropTypes.objectOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      avatar: PropTypes.string,
-      username: PropTypes.string.isRequired,
-    })
-  ),
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired,
+  }),
   files: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
--- src/Components/Post/PostPresenter.js
+++ src/Components/Post/PostPresenter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import TextareaAutosize from "react-autosize-textarea";
 import FatText from "../FatText";
 import Avatar from "../Avatar";
 import { HeartFull, HeartEmpty, Comment } from "../Icons";
@@ -27,10 +28,27 @@ const Location = styled.span`
   font-size: 12px;
 `;
 
-const Files = styled.div``;
+const Files = styled.div`
+  position: relative;
+  position: relative;
+  padding-bottom: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  flex-shrink: 0;
+`;
 
 const File = styled.img`
   max-width: 100%;
+  width: 100%;
+  height: 600px;
+  position: absolute;
+  top: 0;
+  background-image: url(${props => props.src});
+  background-size: cover;
+  background-position: center;
+  opacity: ${props => (props.showing ? 1 : 0)};
+  transition: opacity .5s linear;
 `;
 
 const Button = styled.span`
@@ -61,6 +79,16 @@ const Timestamp = styled.span`
   border-bottom: ${(props) => props.theme.lightGreyColor} 1px solid;
 `;
 
+const Textarea = styled(TextareaAutosize)`
+  border: none;
+  width: 100%;
+  resize: none;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
 export default ({
   user: { username, avatar },
   location,
@@ -68,6 +96,8 @@ export default ({
   isLiked,
   likeCount,
   createdAt,
+  newComment,
+  currentItem,
 }) => (
   <Post>
     <Header>
@@ -78,7 +108,7 @@ export default ({
       </UserColumn>
     </Header>
     <Files>
-      {files && files.map((file) => <File id={file.id} src={file.url} />)}
+      {files && files.map((file, index) => <File key={file.id} id={file.id} src={file.url} showing={index === currentItem} />)}
     </Files>
     <Meta>
       <Buttons>
@@ -89,6 +119,7 @@ export default ({
       </Buttons>
       <FatText text={likeCount === 1 ? "1 like" : `${likeCount} likes`} />
       <Timestamp>{createdAt}</Timestamp>
+      <Textarea placeholder={"Add a comment..."} {...newComment} />
     </Meta>
   </Post>
 );
